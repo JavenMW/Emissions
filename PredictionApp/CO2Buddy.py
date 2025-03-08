4547cace975647fcbdfe746da90e4964e4875466
Javen Walker <javenwalker1996@outlook.com>
2025-03-07 20:28:14 -0600

    Automatically loaded the file from our WD.

--- PredictionApp/CO2Buddy.py
+++ PredictionApp/CO2Buddy.py
@@ -11,7 +11,7 @@ def load_model():
     """
     # model = st.file_uploader('Load the')
     # path = (f'{filedialog.askopenfilename(filetypes=[('Pickle files', '*.pkl')])}')
-    path = 'model.pkl'
+    path = 'Emissions/PredictionApp/model.pkl'
     
     try:
         with open(path, 'rb') as f:
